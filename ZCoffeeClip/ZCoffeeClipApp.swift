83726ad516068ca5904391b6aa2c65e4a0389c29
3dlifeStudio <mjfernandez3d@gmail.com>
2021-06-18 15:38:25 +0100

    Initial Commit

---
+++ ZCoffeeClip/ZCoffeeClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZCoffeeClipApp.swift
+//  ZCoffeeClip
+//
+//  Created by Mario Fernandes on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZCoffeeClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
